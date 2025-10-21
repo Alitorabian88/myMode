958c90cbc8d42ec1f14f2eba95408ef80189617b
Ali Torabian <ali.torabian@bechtle.com>
2025-10-21 13:35:52 +0200

    install : tamagui and nativewind

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
-import {StatusBar, Text, useColorScheme, View} from 'react-native';
-import {
-  SafeAreaProvider,
-} from 'react-native-safe-area-context';
-import { makeStyles } from './App.styles';
+import "./global.css";
+import { StatusBar, Text, useColorScheme, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -16,12 +14,13 @@ function App() {
 }
 
 function AppContent() {
-  const isDarkMode = useColorScheme() === 'light';
-  const styles = makeStyles(isDarkMode);
   return (
-    <View style={styles.container}>
-     <Text style={styles.title}>myMode</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
+
 export default App;
