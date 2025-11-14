e274a073b1e434a899437886b871d61bc3fc627c
Ali Torabian <ali.torabian@bechtle.com>
2025-11-15 00:06:54 +0100

    install dependency

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
-  plugins: ['react-native-reanimated/plugin'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+          },
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        },
+      ],
+      // NOTE: Reanimated plugin must be listed last
+      'react-native-reanimated/plugin',
+    ],
+  };
 };
