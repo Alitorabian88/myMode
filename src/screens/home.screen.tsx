326a9d52cce6d1ccfd8131d7ec83ebf8ca144f9f
Ali Torabian <ali.torabian@bechtle.com>
2025-11-15 00:07:30 +0100

    create Home screen

---
+++ src/screens/home.screen.tsx
@@ -0,0 +1,13 @@
+// Package
+import {View, Text} from 'react-native'
+import React, {FC} from 'react'
+
+const HomeScreen: FC = () => {
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
