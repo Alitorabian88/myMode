a9d4c136d1a5d97f5524e55a42f6e45cccc49646
Ali Torabian <ali.torabian@bechtle.com>
2025-11-15 00:08:17 +0100

    create a welcome screen component

---
+++ src/components/welcome/index.tsx
@@ -0,0 +1,3 @@
+import Welcome from "@/components/welcome/welcome";
+
+export  {Welcome};
---
+++ src/components/welcome/welcome.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+import React, {FC} from 'react'
+
+const Welcome:FC = () => {
+    return (
+        <View>
+            <Text>Welcome</Text>
+        </View>
+    )
+}
+export default Welcome;
